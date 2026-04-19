fc4cdb5d3e98396d75075e5deabe170781587338
Saurabh Singh <saurabhsraghuvanshi@gmail.com>
2026-04-19 14:45:50 +0530

    Update gatsby-config.js
    Signed-off-by: Saurabh Singh <saurabhsraghuvanshi@gmail.com>

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
   siteMetadata: {
     title: "Meshery Shape Builder",
     siteUrl: "https://shapes.meshery.io",
-  },
+  },  
   plugins: [
     "gatsby-plugin-styled-components",
     {
